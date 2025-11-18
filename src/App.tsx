2a26f5c5d6937ab6e7de5382fc456b18e0737855
vpg-99 <vishnupriyagupta93@gmail.com>
2025-11-18 10:21:34 +0530

    added the headers

--- src/App.tsx
+++ src/App.tsx
@@ -175,6 +175,18 @@ export default function App() {
   return (
     <div className="min-h-screen bg-gray-50 p-8">
       <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center mb-6">
+          <div className="flex-col">
+            <h1 className="text-3xl font-bold text-black">User Details</h1>
+            <p className="text-sm text-gray-500 mt-2 font-medium">
+              Information about a user, including name, email, start date,
+              inviter, status, and available actions.
+            </p>
+          </div>
+          <button className="bg-gray-950 text-white px-4 py-2 rounded-md">
+            Download Report
+          </button>
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-6">
           <StatCard
             title="Total Users"
